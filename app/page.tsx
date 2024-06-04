900aa1871327012de24ed08d9471094e21b745e2
matheusavi <matheusavi@users.noreply.github.com>
2024-06-04 05:19:30 +0000

    deploy: 0a4c17b23c7dab5227f4aef34d945069ae655efe

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-import Map from './components/map';
-export default function Home() {
-  return (
-    <main className='flex min-h-screen flex-col items-center p-8'>
-      <Map />
-    </main>
-  );
-}
